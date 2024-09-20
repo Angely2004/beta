fa2fe5aca45bb6babb349099eeec0f5ed0407b40
Angely <valechaves2004@gmail.com>
2024-09-20 12:16:53 -0500

    Endpoint : Database connection

---
+++ api/config/db_connection.php
@@ -0,0 +1,32 @@
+<?php
+/* PostgreSQL database connection 
+developer: Angely 
+*/
+
+$host = "localhost"; // 127.0.0.1
+$username = "postgres";
+$password = "unicesmag";
+$dbname = "beta";
+$port = "5432";
+
+// crear string de conexiones
+$data_connection = "
+host=$host 
+port=$port 
+dbname=$dbname 
+user=$username 
+password=$password";
+
+// crear la conexion
+$conn = pg_connect($data_connection);
+
+// verificar la conexion
+if (!$conn) {
+    die("connection failed: " . pg_last_error());
+} else {
+    echo "Connected successfully";
+}
+
+// cerrar la conexion
+pg_close($conn);
+?>
